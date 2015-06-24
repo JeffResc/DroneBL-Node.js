a61a6c300b28ec283057ece0b94399a105b6bcab
AlphaTech <alphatech@btri.x10.bz>
2015-06-24 15:26:19 -0400

    Version 1.0.3

--- README.md
+++ README.md
@@ -6,13 +6,17 @@ Example Lookup:
 First run ```npm install dronebl``` to install the DroneBL package to your system.
 
 ```javascript
-var DroneBL = require('DroneBL');
+var DroneBL = require('dronebl');
 
-DroneBL.lookup("8.8.8.8", function(res) {
-    console.log(res); //Returns false since 8.8.8.8 is not listed
+dronebl.lookup("8.8.8.8", function(res) {
+	if (res.status != 'failure') {
+		console.log(res.result); //Returns false since 8.8.8.8 is not listed
+	}
 });
 
 DroneBL.lookup("213.163.51.164", function(res) {
-    console.log(res); //Returns true since 213.163.51.164 is not listed
+	if (res.status != 'failure') {
+		console.log(res.result); //Returns true since 213.163.51.164 is not listed
+	}
 });
 ```
--- index.js
+++ index.js
@@ -3,14 +3,33 @@ var request = require('request');
 module.exports = {
     lookup: function(ip, cb) {
         var bytes = ip.split(".");
-        request('http://' + bytes[3] + '.' + bytes[2] + '.' + bytes[1] + '.' + bytes[0] + '.dnsbl.dronebl.org', function (error, response, body) {
-            if (error.code == 'ENOTFOUND') {
-                cb('false');
-            } else if (error.code == 'ECONNREFUSED') {
-                cb('true');
-            } else {
-                cb('error')
-            }
-        });
+        var byte0 = bytes[0];
+		var byte1 = bytes[1];
+		var byte2 = bytes[2];
+		var byte3 = bytes[3];
+		if (typeof byte0 == 'undefined' || typeof byte1 == 'undefined' || typeof byte2 == 'undefined' || typeof byte3 == 'undefined') {
+			var obj = {
+				"status": "failure",
+				"message": "IPv4 Address is Not Properly Formatted"
+			}
+		} else {
+			request('http://' + byte3 + '.' + byte2 + '.' + byte1 + '.' + byte0 + '.dnsbl.dronebl.org', function (error, response, body) {
+				if (error.code == 'ENOTFOUND') {
+					var obj = {
+						"status": "success",
+						"result": "false"
+					}
+				} else if (error.code == 'ECONNREFUSED') {
+					var obj = {
+						"status": "success",
+						"result": "true"
+					}
+				} else {
+						"status": "failure",
+						"message": "Unknown error""
+				}
+			});
+		}
+		cb(obj);
     }
 }
