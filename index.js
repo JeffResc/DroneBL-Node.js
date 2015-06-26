22e6057c8530ebefbb542b73d7fddd5538a58078
AlphaTech <AlphaT3ch@icloud.com>
2015-06-26 01:59:34 -0400

    Update for IPv6

--- index.js
+++ index.js
@@ -2,15 +2,25 @@ var request = require('request');
 
 module.exports = {
     lookup: function(ip, cb) {
-        var bytes = ip.split(".");
-        request('http://' + bytes[3] + '.' + bytes[2] + '.' + bytes[1] + '.' + bytes[0] + '.dnsbl.dronebl.org', function (error, response, body) {
-            if (error.code == 'ENOTFOUND') {
+	if (ip.indexOf(':') == -1) {
+        request('http://' + ip.split(':').reverse().join(':') + '.dnsbl.dronebl.org', function (error, response, body) {
+            if (error.code === 'ENOTFOUND') {
                 cb('false');
-            } else if (error.code == 'ECONNREFUSED') {
+            } else if (error.code === 'ECONNREFUSED') {
                 cb('true');
             } else {
                 cb('error')
             }
         });
+	} else {
+	request('http://' + ip.split(':').reverse().join(':') + '.dnsbl.dronebl.org', function (error, response, body) {
+	    if (error.code === 'ENOTFOUND') {
+		cb('false');
+	    } else if (error.code === 'ECONNREFUSED') {
+		cb('true');
+	    } else {
+		cb('error');
+	    }
+	}
     }
 };
