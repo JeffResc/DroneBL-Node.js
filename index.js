04b3a2ac4abdbf373ef6728a96ca36e0e7f76b23
Michał Nerć <aylard7@gmail.com>
2016-10-04 21:33:47 +0200

    Added IPv6 support

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ let request = require('request')
 
 // Module object
 let DroneBL = {
+  resolveIP: (ip) => {
+    let ipv4Re = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
+    let ipv6Re = /^((?:[0-9A-Fa-f]{1,4}))((?::[0-9A-Fa-f]{1,4}))*::((?:[0-9A-Fa-f]{1,4}))((?::[0-9A-Fa-f]{1,4}))*|((?:[0-9A-Fa-f]{1,4}))((?::[0-9A-Fa-f]{1,4})){7}$/
+    if(ip.match(ipv4Re))
+      return ip.split('.').reverse().join('.')
+    if(ip.match(ipv6Re))
+      return ip.split(':').reverse().join(':')
+  }
   resolveResponse: (code, cb) => {
     switch (code) {
       case 'ENOTFOUND': cb('false'); break;
@@ -13,15 +21,9 @@ let DroneBL = {
     }
   },
   lookup: (ip, cb) => {
-    if (ip.indexOf(':') == -1) {
-      request('http://' + ip.split(':').reverse().join(':') + '.dnsbl.dronebl.org', (error, response, body) => {
-        DroneBL.resolveResponse(error.code, cb)
-      });
-    } else {
-      request('http://' + ip.split(':').reverse().join(':') + '.dnsbl.dronebl.org', (error, response, body) => {
-        DroneBL.resolveResponse(error.code, cb)
-      })
-    }
+    request('http://' + DroneBL.resolveIP(ip) + '.dnsbl.dronebl.org', (error, response, body) => {
+      DroneBL.resolveResponse(error.code, cb)
+    });
   }
 }
 
