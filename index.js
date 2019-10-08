c325a3fa36180ec55f8b21e444e96ad79dfefaae
Danielle McCarthy <danielle.mccarthy@levvel.io>
2019-10-08 17:03:24 -0400

    removed request dependency

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
 'use strict';
 
 // Requirements
-let request = require('request')
+let https = require('https');
 
 // Module object
 let DroneBL = {
-  resolveIP: (ip) => {
-    let ipv4Re = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
-    let ipv6Re = /^((?:[0-9A-Fa-f]{1,4}))((?::[0-9A-Fa-f]{1,4}))*::((?:[0-9A-Fa-f]{1,4}))((?::[0-9A-Fa-f]{1,4}))*|((?:[0-9A-Fa-f]{1,4}))((?::[0-9A-Fa-f]{1,4})){7}$/
-    if(ip.match(ipv4Re))
-      return ip.split('.').reverse().join('.')
-    if(ip.match(ipv6Re))
-      return ip.split(':').reverse().join(':')
+  resolveIP: ip => {
+    let ipv4Re = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    let ipv6Re = /^((?:[0-9A-Fa-f]{1,4}))((?::[0-9A-Fa-f]{1,4}))*::((?:[0-9A-Fa-f]{1,4}))((?::[0-9A-Fa-f]{1,4}))*|((?:[0-9A-Fa-f]{1,4}))((?::[0-9A-Fa-f]{1,4})){7}$/;
+    if (ip.match(ipv4Re))
+      return ip
+        .split('.')
+        .reverse()
+        .join('.');
+    if (ip.match(ipv6Re))
+      return ip
+        .split(':')
+        .reverse()
+        .join(':');
   },
   resolveResponse: (code, cb) => {
     switch (code) {
-      case 'ENOTFOUND': cb('false'); break;
-      case 'ECONNREFUSED': cb('true'); break;
-      default: cb('error'); break;
+      case 'ENOTFOUND':
+        cb('false');
+        break;
+      case 'ECONNREFUSED':
+        cb('true');
+        break;
+      default:
+        cb('error');
+        break;
     }
   },
   lookup: (ip, cb) => {
-    request('http://' + DroneBL.resolveIP(ip) + '.dnsbl.dronebl.org', (error, response, body) => {
-      DroneBL.resolveResponse(error.code, cb)
-    });
-  }
-}
+    https.get(
+      'http://' + DroneBL.resolveIP(ip) + '.dnsbl.dronebl.org',
+      (error, response, body) => {
+        DroneBL.resolveResponse(error.code, cb);
+      }
+    );
+  },
+};
 
 // Export module
-module.exports = DroneBL
+module.exports = DroneBL;
